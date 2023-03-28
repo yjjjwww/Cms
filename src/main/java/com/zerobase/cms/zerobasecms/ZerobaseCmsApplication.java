7d40947f6a7e75d7b18e56e50f2c860751ce2c50
yjjjwww <yjjjwww@naver.com>
2023-03-28 13:09:10 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/cms/zerobasecms/ZerobaseCmsApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.cms.zerobasecms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerobaseCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerobaseCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/cms/zerobasecms/ZerobaseCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.cms.zerobasecms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerobaseCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
